d0aac05f66e5e124cadf09cdf39a198ce9d0724d
CyberJoel1 <joel_sec111997@hotmail.com>
2023-01-04 11:51:33 -0500

    cambios subida

--- publication/dto/update-publication.input.ts
+++
@@ -1,8 +0,0 @@
-import { CreatePublicationInput } from './create-publication.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
-
-@InputType()
-export class UpdatePublicationInput extends PartialType(CreatePublicationInput) {
-  @Field(() => Int)
-  id: number;
-}
