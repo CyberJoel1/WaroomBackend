122f2603079092d91ea01a62e908a91ea1a69521
CyberJoel1 <joel_sec111997@hotmail.com>
2023-02-03 02:31:19 -0500

    subir cambios

--- src/neo4j/neo4j.resolver.ts
+++
@@ -1,9 +0,0 @@
-import { Query, Resolver } from '@nestjs/graphql';
-
-@Resolver()
-export class Neo4jResolver {
-    @Query(() => String, { name: 'todos' })
-    helloWorld() {
-      return 'Hello World!';
-    }
-}
--- src/publication/enums/category.publication.ts
+++
@@ -1,8 +0,0 @@
-import { registerEnumType } from "@nestjs/graphql";
-
-export enum validTypesPublication {
-    arriendo     = 'arriendo', 
-    venta        = 'venta',
-}
-
-registerEnumType( validTypesPublication, { name: 'validTypesPublication', description: 'Tipos de publicaciones' } )
